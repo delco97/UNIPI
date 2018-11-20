bffdc5629a03a7ba9cccaaa04d8e5ca1a4716ee7
Andrea Del Corto <andrea.delcorto@gmail.com>
2018-11-20 14:14:23 +0100

    Removed unused old code

--- Algo/Lab2017_18/L2/es8/src/es8.c
+++ Algo/Lab2017_18/L2/es8/src/es8.c
@@ -14,19 +14,8 @@
 #define DIM_STR 1001
 //Prototypes
 int my_strcmp(char* s1, char* s2);
+
 //Implementations
-char* my_strcat(char *s1, char *s2){
-
-	int i=0,j=0;
-
-	while(s1[i]!='\0')i++;//Find end of s1
-	//Append s2 on s1 overwriting '\0' of s1.
-	while(s2[j]!='\0'){s1[i]=s2[j];i++;j++;}
-	s1[i]='\0';//Add new end-of-string char
-
-	return s1;
-}
-
 int my_strcmp(char* s1, char* s2){
 	int i=0,res=0;
 	while(res==0 && s1[i]!='\0' && s2[i]!='\0'){
