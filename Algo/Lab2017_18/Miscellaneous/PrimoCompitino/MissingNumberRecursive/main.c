bf30d231bd52fc25b5a39b958f71509be401e2c0
Andrea Del Corto <andrea.delcorto@gmail.com>
2018-12-28 16:02:37 +0100

    Added function to reach T(n) = Θ(log(k)) k is the missing number

--- Algo/Lab2017_18/Miscellaneous/PrimoCompitino/MissingNumberRecursive/main.c
+++ Algo/Lab2017_18/Miscellaneous/PrimoCompitino/MissingNumberRecursive/main.c
@@ -17,7 +17,10 @@ L’output contiene gli elementi dell’array ordinato, uno per riga.
 void printStrings(char **a,int dim){
 	for (int i = 0; i < dim; i++)printf("%s\n",a[i]);
 }
-
+/*
+Find the missing number in an array with distinct elemnts, sorted in increasing order (<).
+All the items must be in the following range; [1;n+1].
+*/
 int findMissing(int * a,int sx,int dx,int n){
   if(sx > dx)return n; //Base case empty subarray
   if(sx == dx){//Base case: 1 element
@@ -28,6 +31,18 @@ int findMissing(int * a,int sx,int dx,int n){
     if(a[sx] - 1 == sx)return findMissing(a,cx + 1,dx, n);
     else return findMissing(a,sx,cx, n);
   }
+}
+
+/*
+ A way to perform findMissing in θ(log(k))
+*/
+int findMissing_k(int * a,int sx,int dx,int n){
+	int i=0;
+	while(i<n && a[i] - 1 == i)i = 2 * i;
+	if(i>=n)//Array lopped without encountering a[i] - 1 != i => The missing number could be found in [(i/2) + 1;n-1]
+		return findMissing(a,(i/2)+1,n-1,n);
+	else //Array lopped interrupted due to a[i] - 1 == i => The missing number could be found in [(i/2) + 1;i]
+		return findMissing(a,(i/2)+1,i,n);
 }
 
 int main(void) {
@@ -45,7 +60,7 @@ int main(void) {
         else{
             for(int i=0;i<n1;i++)scanf("%d",&a1[i]);
 
-            printf("Missing number: %d\n",findMissing(a1,0,n1-1,n1));
+            printf("Missing number: %d\n",findMissing_k(a1,0,n1-1,n1));
 
             free(a1);
         }
