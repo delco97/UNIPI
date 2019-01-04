d555ad744257f19e249af1c1b148b0ea044626f9
Andrea Del Corto <andrea.delcorto@gmail.com>
2019-01-04 14:30:12 +0100

    Init exercise

---
+++ Algo/Lab2017_18/L7/Es7.2_PuntiColorati/Es7.2_PuntiColorati/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Es7.2_PuntiColorati
+//
+//  Created by Andrea Del Corto on 04/01/19.
+//  Copyright © 2019 Andrea Del Corto. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
