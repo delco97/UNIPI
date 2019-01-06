46d4a7f739f49a06022b28d7ce66611a0f0755c9
Andrea Del Corto <andrea.delcorto@gmail.com>
2019-01-06 15:48:22 +0100

    updated

--- Algo/Lab2017_18/L7/Es7.3_AnagrammaPrincipale/Es7.3_AnagrammaPrincipale/main.c
+++ Algo/Lab2017_18/L7/Es7.3_AnagrammaPrincipale/Es7.3_AnagrammaPrincipale/main.c
@@ -7,9 +7,75 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_CHAR 20
+
+int comapareChar(const void * a,const void * b){
+    char c1 = *((char *)a);
+    char c2 = *((char *)b);
+    return c1 - c2;
+}
+
+void printStrings(char ** a,int n){
+    for(int i=0;i<n;i++)printf("%s\n",a[i]);
+}
+
+void deallocationStrings(char ** a,int k){
+    if(a!=NULL){
+        for(int i=0;i<=k;i++)free(a[i]);
+        free(a);
+    }
+}
+
+char ** allocationStrings(int n,int max_char){
+    char ** aux = NULL;
+    aux = (char **)malloc(n * sizeof(char *));
+    if(aux == NULL)printf("Bad allocation (1)");
+    else{//Good allocation
+        for (int i=0; i<n; i++) {
+            aux[i] = (char *)malloc(max_char * sizeof(char *));
+            if(aux[i] == NULL){
+                printf("Bad allocation (2)");
+                deallocationStrings(aux,i-1);
+                aux = NULL;
+                break;
+            }
+        }
+    }
+    return aux;
+}
+
+char ** readStrings(int * size){
+    char ** aux = NULL;
+    int n = 0;
+    
+    scanf("%d",&n);
+    if(n<=0)printf("n must be greater then 0.");
+    else{
+        aux = allocationStrings(n,MAX_CHAR);
+        if(aux != NULL)//Good allocation
+            for(int i=0;i<n;i++)scanf("%s",aux[i]); //Read strings
+    }
+    
+    *size = n;
+    return aux;
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    char ** inputStrings;
+    int n;
+    
+    inputStrings = readStrings(&n);
+    if(inputStrings == NULL)printf("Read went wrong.");
+    else{//Good read
+        //Sort each string charcater
+        for(int i=0;i<n;i++) qsort(inputStrings[i], strlen(inputStrings[i]), sizeof(char), comapareChar);
+        //printf("\nSorted strings: \n");printStrings(inputStrings,n); //TEST
+        
+    }
+    
+    
+    
     return 0;
 }
