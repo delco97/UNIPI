344eb2a366780f91141210f662a9954ff4587945
Andrea Del Corto <andrea.delcorto@gmail.com>
2019-01-07 12:47:15 +0100

    Init Es8.3

---
+++ Algo/Lab2017_18/L8/Es8.3_Move-to-Front/Es8.3_Move-to-Front/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Es8.3_Move-to-Front
+//
+//  Created by Andrea Del Corto on 07/01/19.
+//  Copyright © 2019 Andrea Del Corto. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
