6e9a8b3e9e2801fb6732d1176ca73aad47f71f31
Andrea Del Corto <andrea.delcorto@gmail.com>
2019-01-07 15:12:11 +0100

    Init Es8.4

---
+++ Algo/Lab2017_18/L8/Es8.4_ListaBidirezionale_Conteggio/Es8.4_ListaBidirezionale_Conteggio/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Es8.4_ListaBidirezionale_Conteggio
+//
+//  Created by Andrea Del Corto on 07/01/19.
+//  Copyright © 2019 Andrea Del Corto. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
