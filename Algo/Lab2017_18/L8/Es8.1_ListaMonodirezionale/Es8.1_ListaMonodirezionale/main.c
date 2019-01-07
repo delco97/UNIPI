6fce0cf32c1aa2541b1af996ad4f6c5c9f1f6449
Andrea Del Corto <andrea.delcorto@gmail.com>
2019-01-07 11:13:11 +0100

    Read input

--- Algo/Lab2017_18/L8/Es8.1_ListaMonodirezionale/Es8.1_ListaMonodirezionale/main.c
+++ Algo/Lab2017_18/L8/Es8.1_ListaMonodirezionale/Es8.1_ListaMonodirezionale/main.c
@@ -7,13 +7,46 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct _Node{//Nodo per lista unidirezionale
     int val;
     struct _Node * next;
 } Node;
 
-int main(int argc, const char * argv[]) {
+void addBottom(Node ** h,int k){
+    Node * curs = NULL;
+    Node * aux = malloc(sizeof(Node));
+    
+    aux->val = k;
+    aux->next = NULL;
+    if(*h == NULL){//Empty list
+        *h = aux;
+    }else{//At least one element in list
+        //Loop list
+        curs = *h;
+        while(curs->next != NULL)curs=curs->next; //Loop untill the last elment (curs->next == NULL) is reached
+        curs->next = aux;
+    }
+}
 
+Node * readNodes(int * n){
+    Node * head = NULL;
+    int val;
+    
+    scanf("%d",n);//Read number of elements
+    for(int i = 0;i < *n;i ++){
+        scanf("%d",&val);
+        addBottom(&head,val);
+    }
+    return head;
+}
+
+int main(int argc, const char * argv[]) {
+    Node * head = NULL;
+    int n;
+    
+    head  = readNodes(&n);
+    
     return 0;
 }
