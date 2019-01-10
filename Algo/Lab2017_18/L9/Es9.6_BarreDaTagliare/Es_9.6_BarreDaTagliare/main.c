10ddb6cb147fe70fbfff076a4388c4fd48b96f5d
Andrea Del Corto <andrea.delcorto@gmail.com>
2019-01-10 16:47:54 +0100

    solution

--- Algo/Lab2017_18/L9/Es9.6_BarreDaTagliare/Es_9.6_BarreDaTagliare/main.c
+++ Algo/Lab2017_18/L9/Es9.6_BarreDaTagliare/Es_9.6_BarreDaTagliare/main.c
@@ -27,6 +27,23 @@ int * readBarsPrices(int * dim){
     return a;
 }
 
+int getMaxProfit_OLD(int * a,int n,int sx,int dx,int  max){
+    if(dx < sx) return 0; //Price for no bar
+    if(sx == dx) return a[dx-sx]; //price for bar long 1
+    //Bar is long more then 1
+    int cx = (sx + dx)/2; //Cut bar in the middle
+    int lenSx = (cx - sx) + 1; //length left bar (at least is 1)
+    int lenDx = (dx - (cx + 1)) + 1; //length right bar (at least is 1)
+    if( (a[lenSx-1] + a[lenDx-1]) > max) //Check if the price of barSx + barDx is bigger then the current max price
+        max = (a[lenSx-1] + a[lenDx-1]);
+    int maxSx = getMaxProfit(a,n,sx,cx,max); //Get the max profit of the left bar
+    int maxDx = getMaxProfit(a,n,cx + 1, dx,max); //Get the max profit of the right bar
+    if( maxSx + maxDx > max) max = maxSx + maxDx;
+    return max;
+}
+
+
+void print
 
 
 int main(int argc, const char * argv[]) {
@@ -35,7 +52,10 @@ int main(int argc, const char * argv[]) {
     
     int n; //Lenght of the bar to cut. (*) I know the prices of all the bars less then n;
     prices = readBarsPrices(&n);
-
+    
+    if(n>0){//Bar is at least 1 inch long
+        printf("\n%d",getMaxProfit(prices,n,0,n-1,prices[n-1]));
+    }
     
     return 0;
 }
