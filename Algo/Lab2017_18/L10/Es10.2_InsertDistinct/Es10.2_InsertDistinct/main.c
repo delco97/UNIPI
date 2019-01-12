d502a58d6e6d5f3667da43c0f52d4c4c0e07a567
Andrea Del Corto <andrea.delcorto@gmail.com>
2019-01-12 11:27:42 +0100

    Solved rod problem. (Cormen help)

---
+++ Algo/Lab2017_18/L10/Es10.2_InsertDistinct/Es10.2_InsertDistinct/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Es10.2_InsertDistinct
+//
+//  Created by Andrea Del Corto on 10/01/19.
+//  Copyright © 2019 Andrea Del Corto. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
