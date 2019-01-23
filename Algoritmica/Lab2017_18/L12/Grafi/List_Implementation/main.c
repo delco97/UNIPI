b370c7f289ea5bd00631f94f3e7bb9cffea23113
Andrea Del Corto <andrea.delcorto@gmail.com>
2019-01-23 13:31:46 +0100

    Code restyle

--- Algoritmica/Lab2017_18/L12/Grafi/List_Implementation/main.c
+++ Algoritmica/Lab2017_18/L12/Grafi/List_Implementation/main.c
@@ -1,36 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct _Node{
+const char * colors[] = {"R","B","W"};
+typedef enum{
+	R = 0, //red 
+	B = 1, //blue
+	W = 2 //white
+} Color;
+
+typedef struct _LNode{
 	int k;
-   struct _Node * next;
-	struct _Node * prev;
-} Node;
+   struct _LNode * next;
+	struct _LNode * prev;
+} LNode;
 
 typedef struct _List{
-	Node * h; //Head pointer
-	Node * t; //Tail pointer
-   int n; //Number of elements in list
+	LNode * h; //Head pointer
+	LNode * t; //Tail pointer
+	int n; //Number of item in list
 } List;
 
+typedef struct _GNode{
+	List adj; //Adjacency list
+   Color c; //Color
+	int d; //Discovery time
+	int f; //End visit time
+} GNode;
+
 typedef struct _Graph{
-    List * a; //Adjacency list
+    GNode * a; //Array with graph nodes
     int n;  //Nodes number
 } Graph;
 
 
-void readList(List * list){
+void readGNode(List * list){
 	List l;
    int k; 
 	//Initialization
 	l.h = NULL; 
 	l.t = NULL;
 	//Read number of arches/nodes expected
-	scanf("%d",&l.n);// Number of arches to read	
+	scanf("%d",&l.n);// Number of departing arches to read	
 	for(int i=0;i<l.n;i++){
-		Node * aux;
+		LNode * aux;
 		scanf("%d",&k);
-		aux = (Node *) malloc(sizeof(Node));
+		aux = (LNode *) malloc(sizeof(LNode));
 		aux->k = k;
 		aux->next = NULL;
 		aux->prev = l.t;
@@ -50,20 +64,24 @@ int readGraph(Graph * graph){
 	Graph g;
 
 	scanf("%d",&g.n); //Read number of nodes
-	g.a = (List *) malloc(g.n * sizeof(List));
+	g.a = (GNode *) malloc(g.n * sizeof(GNode));
 	//Read arches for each node
-   for(int i=0;i<g.n;i++)
-		readList(&(g.a[i]));
+   for(int i=0;i<g.n;i++){
+		readGNode(&(g.a[i].adj)); //Read adjacency list
+		g.a[i].c = W;
+		g.a[i].d = 0;
+		g.a[i].f = 0;
+	}
 
 	*graph = g;
 	return 1;
 }
 
 void printGraph(Graph g){
-	Node * cur = NULL;
+	LNode * cur = NULL;
 	for (int i=0; i<g.n; i++) {
-		printf("%d: ",i);
-      cur = g.a[i].h; 
+		printf("%d [%s] (%d/%d): ",i,colors[g.a[i].c],g.a[i].d,g.a[i].f);
+      cur = g.a[i].adj.h; //Head of the adjacency list of node i
 		while(cur != NULL){
 			printf("%d -> ",cur->k);
 			cur = cur->next;
@@ -72,12 +90,13 @@ void printGraph(Graph g){
 	}
 }
 
+
 int main(){
     Graph g;
     
     if( readGraph(&g) ){//Good graph read
-        printf("Graph read: \n");
-        printGraph(g);
+		printf("Graph read: \n");
+		printGraph(g);
     }
         
     
