41e28ca1fabe955a91a2d11c4a871f2d64d6bc92
Andrea Del Corto <andrea.delcorto@gmail.com>
2019-02-09 17:19:24 +0100

    Solution

--- Algoritmica/Lab2017_18/L12/Es12.1_GrafoBipartito/main.c
+++ Algoritmica/Lab2017_18/L12/Es12.1_GrafoBipartito/main.c
@@ -1,36 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
-#include <time.h>
-
 
 const char * colors[] = {"R","B","W","G"};
 typedef enum{
-	R = 0, //red 
-	B = 1, //blue
-	W = 2, //white
-	G = 3  //grey
+    R = 0, //red
+    B = 1, //blue
+    W = 2 //white
 } Color;
 
 typedef struct _LNode{
-	int k;
-   struct _LNode * next;
-	struct _LNode * prev;
+    int k;
+    struct _LNode * next;
+    struct _LNode * prev;
 } LNode;
 
 typedef struct _List{
-	LNode * h; //Head pointer
-	LNode * t; //Tail pointer
-	int n; //Number of item in list
+    LNode * h; //Head pointer
+    LNode * t; //Tail pointer
+    int n; //Number of item in list
 } List;
 
 typedef struct _GNode{
-	List adj; //Adjacency list
-   Color c; //Color
-   int id;
-   int prev; //Id node visited before
-	int d; //Discovery time
-	int f; //End visit time
+    List adj; //Adjacency list
+    //BFS attributes
+    Color c; //Color
+    int d; //Discovery time
+    int prev; //Previous nodes discovered
 } GNode;
 
 typedef struct _Graph{
@@ -38,115 +34,174 @@ typedef struct _Graph{
     int n;  //Nodes number
 } Graph;
 
+void deallocateList(List * l){
+	LNode * cur = l->h;
+	LNode * aux;
+	while(cur != NULL){
+		aux = cur;
+		cur = cur->next;
+		free(aux);
+	}
+	l->h = NULL;
+	l->t = NULL;
+}
 
 void readGNode(List * list){
-	List l;
-   int k; 
-	//Initialization
-	l.h = NULL; 
-	l.t = NULL;
-	//Read number of arches/nodes expected
-	scanf("%d",&l.n);// Number of departing arches to read	
-	for(int i=0;i<l.n;i++){
-		LNode * aux;
-		scanf("%d",&k);
-		aux = (LNode *) malloc(sizeof(LNode));
-		aux->k = k;
-		aux->next = NULL;
-		aux->prev = l.t;
-		//Bottom insert in list
-		if(l.t == NULL){//Empty list
-			l.h = aux;
-			l.t = aux;
-		}else{//At least one element in list
-			l.t->next = aux;
-			l.t = aux;
-		}
-   }
-	*list = l;
+    List l;
+    int k;
+    //Initialization
+    l.h = NULL;
+    l.t = NULL;
+    //Read number of arches/nodes expected
+    scanf("%d",&l.n);// Number of departing arches to read
+    for(int i=0;i<l.n;i++){
+        LNode * aux;
+        scanf("%d",&k);
+        aux = (LNode *) malloc(sizeof(LNode));
+        aux->k = k;
+        aux->next = NULL;
+        aux->prev = l.t;
+        //Bottom insert in list
+        if(l.t == NULL){//Empty list
+            l.h = aux;
+            l.t = aux;
+        }else{//At least one element in list
+            l.t->next = aux;
+            l.t = aux;
+        }
+    }
+    *list = l;
 }
 
 int readGraph(Graph * graph){
-	Graph g;
-
-	scanf("%d",&g.n); //Read number of nodes
-	g.a = (GNode *) malloc(g.n * sizeof(GNode));
-	//Read arches for each node
-   for(int i=0;i<g.n;i++){
-		readGNode(&(g.a[i].adj)); //Read adjacency list
-		g.a[i].c = W;
-		g.a[i].d = -1;
-		g.a[i].f = -1;
-		g.a[i].prev = -1;
-		g.a[i].id = i;
-	}
-
-	*graph = g;
-	return 1;
+    Graph g;
+    
+    scanf("%d",&g.n); //Read number of nodes
+    g.a = (GNode *) malloc(g.n * sizeof(GNode));
+    //Read arches for each node
+    for(int i=0;i<g.n;i++){
+        readGNode(&(g.a[i].adj)); //Read adjacency list
+        g.a[i].c = W;
+        g.a[i].d = 0;
+        g.a[i].prev = -1;
+    }
+    
+    *graph = g;
+    return 1;
 }
 
 void printGraph(Graph g){
-	LNode * cur = NULL;
-	for (int i=0; i<g.n; i++) {
-		printf("%d - [%s] [prev: %d] (%d/%d): ",g.a[i].id,colors[g.a[i].c],g.a[i].prev,g.a[i].d,g.a[i].f);
-      cur = g.a[i].adj.h; //Head of the adjacency list of node i
-		while(cur != NULL){
-			printf("%d -> ",cur->k);
-			cur = cur->next;
-		}
-		printf("\n");
-	}
+    LNode * cur = NULL;
+    for (int i=0; i<g.n; i++) {
+        printf("%d [%s] d: %d prev: %d): ",i,colors[g.a[i].c],g.a[i].d,g.a[i].prev);
+        cur = g.a[i].adj.h; //Head of the adjacency list of node i
+        while(cur != NULL){
+            printf("%d -> ",cur->k);
+            cur = cur->next;
+        }
+        printf("\n");
+    }
 }
 
-int DFSbipartite(Graph g,GNode u,int * time){
-	LNode * cur = u.adj.h;
-	Color prevCol;
-	 
-	(*time)++;
-	u.d = *time;
-	u.c = R;
-	prevCol = R;
-	while(cur != NULL){
-		if(g.a[cur->k].c == W){
-			g.a[cur->k].prev = u.id;
-			DFSbipartite(g,g.a[cur->k],time);
-		}
-		cur=cur->next;
-	}
-	(*time)++;
-	u.f = *time;
+void tailInsert(List * q,int k){
+    LNode * aux = malloc(sizeof(LNode));
+    aux->k = k;
+    aux->next = NULL;
+    aux->prev = q->t;
+    if(q->t != NULL)q->t->next = aux;
+    if(q->h == NULL) q->h = aux;
+    q->t = aux;
+    q->n++;
+}
+
+int headRemove(List * q){
+    int u = -1;
+    if(q->h != NULL){
+        u = q->h->k;
+        LNode * aux = q->h->next;
+        if(q->h == q->t) q->t = aux;
+        free(q->h);
+        q->h = aux;
+        q->n--;
+    }
+    return u;
+}
+
+int BFS(Graph g,int s){
+    int isBipartite = 1;
+    int u;
+    LNode * cur = NULL;
+    List q; //Queue (FIFO)
+    q.h = NULL;
+    q.t = NULL;
+    q.n = 0;
+	 //Init source node
+    g.a[s].c = B; //could be R, logic wold not change
+    g.a[s].prev = -1;
+    g.a[s].d = 0;
+    tailInsert(&q,s);
+    while(q.n != 0 && isBipartite == 1){
+        u = headRemove(&q);
+        //printf("%d ",u);
+        cur = g.a[u].adj.h; //Head of the adjacency list of node u
+        while(cur != NULL){//Loop adj nodes of u node
+            if(g.a[cur->k].c == W){//White adj node
+                g.a[cur->k].c = g.a[u].c == B ? R:B;
+                g.a[cur->k].d = g.a[u].d + 1;
+                g.a[cur->k].prev = u;
+                tailInsert(&q,cur->k);
+            }else{
+            	if(g.a[cur->k].c == g.a[u].c) isBipartite = 0;
+            }
+            cur = cur->next;
+        }
+    }
+    deallocateList(&q);
+    return isBipartite;
 }
 
 int isBipartite(Graph g){
-	int time;
-	int s;
-	//rand() % (max_number + 1 - minimum_number) + minimum_number
-	s = rand() % ((g.n-1) + 1 - 0) + 0; //[0,g.n)
-	//Init nodes
-	for (int i=0; i<g.n; i++) {
-		g.a[i].c = W;
-		g.a[i].d = -1;
-		g.a[i].f = -1;
-		g.a[i].prev = -1;
+	 //Init nodes for BFS
+    for(int i=0;i<g.n;i++){
+        g.a[i].c = W;
+        g.a[i].d = INT_MAX;
+        g.a[i].prev = -1;
+    }
+    
+    for(int i=0;i<g.n;i++){
+    	if(g.a[i].c == W){
+    		if(BFS(g,i) == 0) return 0; //g is not bipartite
+    	}
+    }
+    return 1;
+}
+
+void printList(List l){
+    LNode * c = l.h;
+    printf("List: ");
+    while(c != NULL){
+        printf("%d ",c->k);
+        c = c->next;
+    }
+}
+
+void deallocateGraph(Graph * g){
+	for(int i=0;i<g->n;i++){
+		deallocateList(&(g->a[i].adj));
 	}
-	time = 0;
-	for (int i=0; i<g.n; i++) 
-		if(g.a[i].c == W && DFSbipartite(g,g.a[i],&time) == 0) return 0;;
-	
-	return 1;
+	free(g->a);
+	g->a = NULL;
 }
 
 int main(){
-    Graph g;
-    
-    if( readGraph(&g) ){//Good graph read
-		printf("Graph read: \n");
-		printGraph(g);
-		printf("Is bipartite?");
-		printf("%d\n",isBipartite(g));
-		
-    }
-        
-    
-    return 0;
+	Graph g;
+	if( readGraph(&g) ){//Good graph read
+		//printf("Graph read: \n");
+		//printGraph(g);
+		//printf("Is biartite? %s",isBipartite(g) == 1?"YES\n":"NO\n");
+		printf("%d",isBipartite(g));
+		deallocateGraph(&g);
+	}
+	return 0;
 }
+
